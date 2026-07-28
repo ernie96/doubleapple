45ffc62aba99c5cddada1295bca55a51590fa92d
ernie96 <ernie96@users.noreply.github.com>
2026-07-28 06:16:24 -0500

    Add Bridging Headers for Swift and C interoperability

---
+++ Apps/DoubleTalkApp/DoubleTalkApp-Bridging-Header.h
@@ -0,0 +1,12 @@
+/*
+ * DoubleTalkApp-Bridging-Header.h
+ *
+ * Exposes CDoubleTalk C API functions (dtalk_shim.h) to Swift code.
+ */
+
+#ifndef DoubleTalkApp_Bridging_Header_h
+#define DoubleTalkApp_Bridging_Header_h
+
+#include "dtalk_shim.h"
+
+#endif /* DoubleTalkApp_Bridging_Header_h */
---
+++ Apps/DoubleTalkVoiceExtension/DoubleTalkVoiceExtension-Bridging-Header.h
@@ -0,0 +1,12 @@
+/*
+ * DoubleTalkVoiceExtension-Bridging-Header.h
+ *
+ * Exposes CDoubleTalk C API functions (dtalk_shim.h) to Swift code in Extension target.
+ */
+
+#ifndef DoubleTalkVoiceExtension_Bridging_Header_h
+#define DoubleTalkVoiceExtension_Bridging_Header_h
+
+#include "dtalk_shim.h"
+
+#endif /* DoubleTalkVoiceExtension-Bridging_Header_h */
