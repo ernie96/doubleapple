bbb58ceceaa13a2d5d21bf64e7311f1184993d52
ernie96 <ernie96@users.noreply.github.com>
2026-07-28 06:34:51 -0500

    Add DoubleTalkKit bridging header and upload xcodebuild-log artifact on completion

---
+++ Sources/CDoubleTalk/include/DoubleTalkKit-Bridging-Header.h
@@ -0,0 +1,12 @@
+/*
+ * DoubleTalkKit-Bridging-Header.h
+ *
+ * Exposes CDoubleTalk C API functions (dtalk_shim.h) to Swift code inside DoubleTalkKit framework.
+ */
+
+#ifndef DoubleTalkKit_Bridging_Header_h
+#define DoubleTalkKit_Bridging_Header_h
+
+#include "dtalk_shim.h"
+
+#endif /* DoubleTalkKit_Bridging_Header_h */
